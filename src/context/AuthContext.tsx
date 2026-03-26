49b23cc3c0c7967e4927c19f73261cc760b4c78f
Akshay Vishwakarma <akshay.vishwakarma@invennico.com>
2026-03-26 15:15:30 +0530

    fix: auth

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -16,7 +16,7 @@ const getInitialState = (): AuthState => {
       localStorage.removeItem('user');
     }
   }
-  return { user: null, token: null, isAuthenticated: true };
+  return { user: null, token: null, isAuthenticated: false };
 };
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
