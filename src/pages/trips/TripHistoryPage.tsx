865ce883c9f8fa4ebd7ad4abeae825d8f49d01ae
praveensahu26 <praveen.sahu@invennico.com>
2026-04-01 18:58:33 +0530

    phone number issues in tables

--- src/pages/trips/TripHistoryPage.tsx
+++ src/pages/trips/TripHistoryPage.tsx
@@ -133,7 +133,7 @@ export default function TripHistoryPage() {
               <span className="text-[14px] font-medium text-[#1DAFA1] whitespace-nowrap">
                 {trip.rider.name}
               </span>
-              <span className="text-[12px] font-medium text-[#4E616A]">
+              <span className="text-[12px] font-medium text-[#4E616A] text-nowrap">
                 {trip.rider.countryCode} {trip.rider.phone}
               </span>
             </div>
@@ -164,7 +164,7 @@ export default function TripHistoryPage() {
               <span className="text-[14px] font-medium text-[#1DAFA1] whitespace-nowrap">
                 {trip.driver.name}
               </span>
-              <span className="text-[12px] font-medium text-[#4E616A]">
+              <span className="text-[12px] font-medium text-[#4E616A] text-nowrap">
                 {trip.driver.countryCode} {trip.driver.phone}
               </span>
             </div>
@@ -286,17 +286,16 @@ export default function TripHistoryPage() {
                 <button
                   key={tab}
                   onClick={() => handleTabChange(tab)}
-                  className={`px-4 py-2 rounded-sm text-[14px] cursor-pointer font-medium transition-all ${
-                    activeTab === tab
-                      ? tab === 'Assigned' || tab === 'All'
-                        ? 'bg-[#EEFFFD] text-[#1DAFA1] border border-[#1DAFA1]'
-                        : tab === 'In Progress'
-                          ? 'bg-[#FFF3D4] text-[#F6921E] border border-[#F6921E]'
-                          : tab === 'Completed'
-                            ? 'bg-[#EAFFF2] text-[#00A63E] border border-[#00A63E]'
-                            : 'bg-[#FFF6F6] text-[#FF0707] border border-[#FF0707]'
-                      : 'text-[#4E616A] bg-transparent border border-[#DFE6E5]'
-                  }`}
+                  className={`px-4 py-2 rounded-sm text-[14px] cursor-pointer font-medium transition-all ${activeTab === tab
+                    ? tab === 'Assigned' || tab === 'All'
+                      ? 'bg-[#EEFFFD] text-[#1DAFA1] border border-[#1DAFA1]'
+                      : tab === 'In Progress'
+                        ? 'bg-[#FFF3D4] text-[#F6921E] border border-[#F6921E]'
+                        : tab === 'Completed'
+                          ? 'bg-[#EAFFF2] text-[#00A63E] border border-[#00A63E]'
+                          : 'bg-[#FFF6F6] text-[#FF0707] border border-[#FF0707]'
+                    : 'text-[#4E616A] bg-transparent border border-[#DFE6E5]'
+                    }`}
                 >
                   {tab}
                 </button>
